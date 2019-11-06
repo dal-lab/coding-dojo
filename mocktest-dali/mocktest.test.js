e417a090c1367eb95cd0ef37e0ad943d10ab9938
amorfati0310 <lyhy0310@gmail.com>
2019-11-07 00:07:11 +0900

    temp: Subject line (try to keep under 50 characters)
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

---
+++ mocktest-dali/mocktest.test.js
@@ -0,0 +1,13 @@
+//1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
+//2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
+// 3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
+
+const findhighScorer = (answers) => {
+  const highscorer
+  return highscorer
+}
+    
+
+test('answers [1,2,3,4,5] to highscorer =  [1]', () => {
+  expect(findhighScorer([1,2,3,4,5])).toBe([1])
+})
