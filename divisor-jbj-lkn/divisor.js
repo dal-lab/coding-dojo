31a908e876bb7fc6ec4f61c593def8808b903ac8
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2019-11-27 08:07:02 +0900

    코드 재정렬

--- divisor-jbj-lkn/divisor.js
+++ divisor-jbj-lkn/divisor.js
@@ -2,27 +2,21 @@ function divideClear(a, b) {
     return a % b == 0
   }
   
-  function divideClearList(a, b) {
-    
-    result = a.filter(element => divideClear(element, b));
+function divideClearList(a, b) {
+
+result = a.filter(element => divideClear(element, b));
     return result;
-  }
-  
-  const example = function(a,b) {
-    return a-b;
-  }
-  
-  function divideClearSortedList(a, b) {
-    
+}
+
+function divideClearSortedList(a, b) {
+
     dividedClearList = divideClearList(a, b);
-  
+
     if (dividedClearList.length == 0) {
-      return [-1];
+        return [-1];
     }
-    
-    return dividedClearList.sort(example);
+
+    return dividedClearList.sort(ascending);
+}
   
-  }
-  
-  module.exports = {divideClear, divideClearList, divideClearSortedList};
-  
+module.exports = {divideClear, divideClearList, divideClearSortedList};
