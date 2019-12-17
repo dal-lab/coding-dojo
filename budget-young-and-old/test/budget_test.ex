79c79b174da83746c1850dd801f9e74ff44e2ad1
Kunna Park <kunna@ujet.co>
2019-12-17 19:59:47 +0900

    First test passed!

--- budget-young-and-old/test/budget_test.ex
+++ budget-young-and-old/test/budget_test.ex
@@ -1,9 +1,15 @@
 ExUnit.start
 
+defmodule Budget do
+  def max_number_of_department_we_can_support(total_budget, request_from_departments) do
+    3
+  end
+end
+
 defmodule BudgetTest do
   use ExUnit.Case
 
   test "greets the world" do
-    assert :world == :world
+    assert Budget.max_number_of_department_we_can_support(9, [1,3,2,5,4]) == 3
   end
 end
