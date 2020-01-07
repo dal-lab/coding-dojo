ce01da4724b56ad5e61e06101f7bae772b652345
wholemann <wholeman.dev@gmail.com>
2020-01-08 00:37:42 +0900

    Use startsWith method instead of subSet

--- skilltree-csh-mkh/src/test/kotlin/com/codingdojo/skilltree/SolutionTest.kt
+++ skilltree-csh-mkh/src/test/kotlin/com/codingdojo/skilltree/SolutionTest.kt
@@ -3,38 +3,25 @@ package com.codingdojo.skilltree
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
-fun solution(skill: String, skill_trees: Array<String>): Int {
-    val subsets = createSubset(skill)
-    return skill_trees.asSequence()
-            .map { processFiltering(skill, it) }
-            .count {
-                subsets.contains(it) || it.isBlank()
-            }
-}
+fun solution(skill: String, skill_trees: Array<String>) =
+        skill_trees
+                .map { prerequisite(skill, it) }
+                .count {
+                    it.startsWith(skill.first()) || it.isBlank()
+                }
 
-fun processFiltering(skill: String, skill_tree: String) = skill_tree.filter { skill.contains(it) }
-
-tailrec fun createSubset(skill: String, subSet: List<String> = emptyList()): List<String> =
-        when {
-            skill.isEmpty() -> subSet
-            else -> createSubset(skill.dropLast(1), subSet + skill)
-        }
+fun prerequisite(skill: String, skill_tree: String) = skill_tree.filter { it in skill }
 
 class SolutionTest {
 
     @Test
     fun `Count available skill tree in a given skill trees`() {
-        assertThat(solution("CBD", arrayOf<String>("BACDE", "CBADF", "AECB", "BDA"))).isEqualTo(2)
-        assertThat(solution("ABC", arrayOf<String>("EFG"))).isEqualTo(1)
+        assertThat(solution("CBD", arrayOf("BACDE", "CBADF", "AECB", "BDA"))).isEqualTo(2)
+        assertThat(solution("ABC", arrayOf("EFG"))).isEqualTo(1)
     }
 
     @Test
-    fun `Filter a skill in skill tree`() {
-        assertThat(processFiltering("CBD", "BACDE")).isEqualTo("BCD");
-    }
-
-    @Test
-    fun `Create subset of skills`() {
-        assertThat(createSubset("CBD", emptyList())).isEqualTo(listOf<String>("CBD", "CB", "C"))
+    fun `Find prerequisite skill in skill tree`() {
+        assertThat(prerequisite("CBD", "BACDE")).isEqualTo("BCD");
     }
 }
