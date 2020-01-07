7376365a199bf2a3b381445a82df285cf42de908
wholemann <wholeman.dev@gmail.com>
2020-01-08 01:12:03 +0900

    Use regex instead of string filter

--- skilltree-csh-mkh/src/test/kotlin/com/codingdojo/skilltree/SolutionTest.kt
+++ skilltree-csh-mkh/src/test/kotlin/com/codingdojo/skilltree/SolutionTest.kt
@@ -5,12 +5,10 @@ import org.junit.Test
 
 fun solution(skill: String, skill_trees: Array<String>) =
         skill_trees
-                .map { prerequisite(skill, it) }
+                .map { it.replace("""[^${skill}]""".toRegex(), "") }
                 .count {
                     it.startsWith(skill.first()) || it.isBlank()
                 }
-
-fun prerequisite(skill: String, skill_tree: String) = skill_tree.filter { it in skill }
 
 class SolutionTest {
 
@@ -18,10 +16,5 @@ class SolutionTest {
     fun `Count available skill tree in a given skill trees`() {
         assertThat(solution("CBD", arrayOf("BACDE", "CBADF", "AECB", "BDA"))).isEqualTo(2)
         assertThat(solution("ABC", arrayOf("EFG"))).isEqualTo(1)
-    }
-
-    @Test
-    fun `Find prerequisite skill in skill tree`() {
-        assertThat(prerequisite("CBD", "BACDE")).isEqualTo("BCD");
     }
 }
