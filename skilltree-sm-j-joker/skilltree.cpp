145573ad91c71c14b42137c3bb1779c673e4a4c5
g0 <13pft13@gmail.com>
2020-01-07 21:05:46 +0900

    Refactor code using algorithm
    Use count_if in algorithm and bind or lambda.
    
    - http://www.cplusplus.com/reference/algorithm/count_if/
    - https://en.cppreference.com/w/cpp/utility/functional/bind
    - https://en.cppreference.com/w/cpp/language/lambda

--- skilltree-sm-j-joker/skilltree.cpp
+++ skilltree-sm-j-joker/skilltree.cpp
@@ -1,11 +1,13 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <algorithm>
+#include <functional>
 #include <iostream>
 #include <cassert>
-#include "ConsoleApplication1.h"
 
 using namespace std;
+using namespace std::placeholders;
 
 #define assert_equal(expected, actual) \
 	_assert_equal(__LINE__, expected, actual)
@@ -20,31 +22,34 @@ void _assert_equal(int line, int expected, int actual) {
 		<< "actual: " << actual << endl;
 }
 
+bool learn_skill(string skill_tree, char skill, vector<bool>& learned) {
+	size_t pos = skill_tree.find(skill);
+	if (pos == string::npos) {
+		return true;
+	}
+	if (pos > 0 && !learned[pos - 1]) {
+		return false;
+	}
+
+	learned[pos] = true;
+	return true;
+}
+
 bool check_skills(string skill_tree, string skills) {
 	vector<bool> learned('Z' - 'A' + 1);
 	for (size_t i = 0; i < skills.size(); i++) {
-		size_t pos = skill_tree.find(skills[i]);
-		if (pos == string::npos) {
-			continue;
+		if (!learn_skill(skill_tree, skills[i], learned)) {
+			return false;
 		}
-		if (pos > 0) {
-			if (!learned[pos - 1]) {
-				return false;
-			}
-		}
-		learned[pos] = true;
 	}
 	return true;
 }
 
 int solution(string skill_tree, vector<string> skillses) {
-	int answer = 0;
-	for (size_t i = 0; i < skillses.size(); i++) {
-		if (check_skills(skill_tree, skillses[i])) {
-			answer++;
-		}
-	}
-	return answer;
+	return count_if(skillses.begin(), skillses.end(),
+		bind(check_skills, skill_tree, _1));
+	//return count_if(skillses.begin(), skillses.end(),
+	//	[=](auto skills) { return check_skills(skill_tree, skills); });
 }
 
 int main() {
