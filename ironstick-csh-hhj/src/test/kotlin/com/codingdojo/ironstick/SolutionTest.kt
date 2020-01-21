3cdffe4a8f3a8593722301276e94964d560f11d2
wholemann <wholeman.dev@gmail.com>
2020-01-21 21:22:38 +0900

    Add solution2 using fold

--- ironstick-csh-hhj/src/test/kotlin/com/codingdojo/ironstick/SolutionTest.kt
+++ ironstick-csh-hhj/src/test/kotlin/com/codingdojo/ironstick/SolutionTest.kt
@@ -3,23 +3,34 @@ package com.codingdojo.ironstick
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
-fun solution(arrangement: String): Int {
+fun solution1(arrangement: String): Int {
     tailrec fun step(arrangement: String, sticks: Int, total: Int): Int = when {
         arrangement.isEmpty() -> total
-        arrangement.take(1) == "(" -> step(arrangement.drop(1), sticks + 1, total)
-        arrangement.take(1) == "*" -> step(arrangement.drop(1), sticks, total + sticks)
+        arrangement.first() == '(' -> step(arrangement.drop(1), sticks + 1, total)
+        arrangement.first() == '*' -> step(arrangement.drop(1), sticks, total + sticks)
         else -> step(arrangement.drop(1), sticks - 1, total + 1)
     }
 
     return step(arrangement.replace("()", "*"), 0, 0)
 }
 
+fun solution2(arrangement: String): Int =
+        arrangement.replace("()", "*").fold(Pair(0, 0)) { acc, c ->
+            when (c) {
+                '(' -> Pair(acc.first + 1, acc.second)
+                '*' -> Pair(acc.first, acc.second + acc.first)
+                else -> Pair(acc.first - 1, acc.second + 1)
+            }
+        }.second
+
 class SolutionTest {
 
     @Test
     fun `Calculate the total number of truncated bar pieces`() {
-        assertThat(solution("(()())")).isEqualTo(3)
-        assertThat(solution("()")).isEqualTo(0)
-        assertThat(solution("()(((()())(())()))(())")).isEqualTo(17)
+        arrayOf(::solution1, ::solution2).forEach { solution ->
+            assertThat(solution("(()())")).isEqualTo(3)
+            assertThat(solution("()")).isEqualTo(0)
+            assertThat(solution("()(((()())(())()))(())")).isEqualTo(17)
+        }
     }
 }
