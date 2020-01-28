7aa166cbe197b84b7f695ef3fe2f0aad60abfb9f
luckysoo3516 <sumin328030@gmail.com>
2020-01-29 00:33:35 +0900

    Solve 'Phone number' problem using cpp
    solved by SM, J

--- phone-numbers-sm-j/phone_number.cpp
+++ phone-numbers-sm-j/phone_number.cpp
@@ -4,54 +4,34 @@
 
 using namespace std;
 
+bool is_prefix(const string& longer, const string& shorter) {
+    auto result = std::mismatch(shorter.begin(), shorter.end(), longer.begin());
+    if (result.first == shorter.end()) {
+        return true;
+    }
+    return false;
+}
+
 bool solution(vector<string> phone_book) {
-    bool answer = true;
+    for (auto i = 0; i < phone_book.size() - 1; i++) {
+        for (auto j = i+1; j < phone_book.size(); j++) {
 
-    for(int i = 0; i<phone_book.size(); i++){
-        string tmp = phone_book[i];
-        for(int j = 0; j<phone_book.size(); j++) {
-            answer = tmp.find(phone_book[j]);
+            const auto longer = phone_book[i].size() > phone_book[j].size() ? phone_book[i] : phone_book[j];
+            const auto shorter = phone_book[i].size() > phone_book[j].size() ? phone_book[j] : phone_book[i];
 
-            if (answer == false) {
-                return  false;
+            if (is_prefix(longer, shorter)) {
+                return false;
             }
         }
     }
-    return answer;
+    return true;
 }
 
 int main(){
 
-    // Test case #1
-    vector<string> v1;
-    v1.push_back("119");
-    v1.push_back("97674223");
-    v1.push_back("1195524421");
-
-    if (solution(v1) != false) {
-        return -1;
-    }
-
-    // // Test case #1
-    vector<string> v2;
-    v2.push_back("123");
-    v2.push_back("456");
-    v2.push_back("789");
-    //123,456,789
-    if (solution(v2) != true) {
-        return -1;
-    }
-
-    // // Test case #1
-    vector<string> v3;
-    v3.push_back("12");
-    v3.push_back("123");
-    v3.push_back("1235");
-    v3.push_back("567");
-    v3.push_back("88");
-    if (solution(v3) != false) {
-        return -1;
-    }
+    assert(solution({"119", "97674223", "1195524421"}) == false);
+    assert(solution({"123", "456", "789"}) == true);
+    assert(solution({"12", "123", "1235", "567", "88"}) == false);
 
     return 0;
 }
